dbd99f2140e888c4e63334ac77eed48e33e65d74
monte.ohrt <monte.ohrt@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2010-01-18 00:02:33 +0000

    tag beta 7

---
+++ tags/Smarty_3_b7/development/PHPunit/AssignByRefTests.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Smarty PHPunit tests assign_by_ref methode
+* 
+* @package PHPunit
+* @author Uwe Tews 
+*/
+
+/**
+* class for assign_by_ref tests
+*/
+class AssignByRefTests extends PHPUnit_Framework_TestCase {
+    public function setUp()
+    {
+        $this->smarty = SmartyTests::$smarty;
+        SmartyTests::init();
+    } 
+
+    public static function isRunnable()
+    {
+        return true;
+    } 
+
+    /**
+    * test simple assign_by_ref
+    */
+    public function testSimpleAssignByRef()
+    {
+        $bar = 'bar';
+        $this->smarty->assign_by_ref('foo', $bar);
+        $bar = 'newbar';
+        $this->assertEquals('newbar', $this->smarty->fetch('string:{$foo}'));
+    } 
+} 
+
+?>
